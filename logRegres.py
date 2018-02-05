17541da65a281ebfe1a21eb18f00a3f56fe5f4ec
yipwinghong <yipwinghong@outlook.com>
2018-02-05 22:52:41 +0800

    :bug: update logistic regression and perceptron

--- logRegres.py
+++ logRegres.py
@@ -6,44 +6,40 @@ def loadDataSet():
     """ 创建示例数据集 """
 
     dataMat, labelMat = [], []
-    for line in open('testSet.txt').readlines():        # 3列、100行的数据集
+    for line in open('testSet.txt').readlines():            # 3列（表示两个特征）、100行的数据集
         lineArr = line.strip().split()
-        dataMat.append(
-            [1.0, float(lineArr[0]), float(lineArr[1])]
-        )
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])     # 第一个补位
         labelMat.append(int(lineArr[2]))
-    return dataMat, labelMat
+    return array(dataMat), array(labelMat)
 
 
 def sigmoid(inX):
-    """ sigmoid函数 """
+    """
+    sigmoid函数 传入参数为向量内积（输入向量*权值向量）
+    :param inX: 
+    :return: 
+    """
 
     return 1.0 / (1 + exp(-inX))
 
 
-def gradAscent(dataMatIn, classLabels):
+def gradAscent(dataMatIn, classLabels, alpha=0.001, maxCycles=150):
     """
     梯度上升法：每次更新回归系数需要遍历整个数据集，批处理算法
     :param dataMatIn: 
-    :param classLabels: 
+    :param classLabels:
+    :param alpha:       学习率
     :return: 
     """
 
-    alpha = 0.001
-
-    dataMatrix = mat(dataMatIn)                 # 转换100 * 3矩阵：3个特征（2个可用），100行数据
-    labelMat = mat(classLabels).transpose()     # 转换100 * 1矩阵：转置
-    m, n = shape(dataMatrix)                    # 获取矩阵行数、列数
-
-    weights = ones((n, 1))                      # 把权值初始化为3 * 1矩阵
-
-    # TODO 求梯度gradient = dataMatrix.transpose() * (labelMat - sigmoid(dataMatrix * weights))
-    for _ in range(500):                        # 迭代计算500次
-        h = sigmoid(dataMatrix * weights)       # 把训练矩阵*权值矩阵结果（100*3 x 3*1）置入sigmoid函数
-        error = labelMat - h
-        weights = weights + alpha * dataMatrix.transpose() * error  # 梯度上升迭代计算权值
-
-    return weights.getA()                       # 把权值矩阵转化为数组
+    dataMatrix = mat(dataMatIn)                     # 转换100 * 3矩阵：3个特征（2个可用），100行数据
+    labelMat = mat(classLabels).transpose()         # 转换100 * 1矩阵：转置
+    m, n = shape(dataMatrix)                        # 获取矩阵行数、列数
+    weights = ones((n, 1))                                      # 把权值初始化为3 * 1矩阵
+    for _ in range(maxCycles):                                  # TODO 根据偏差调整权值，迭代计算500次
+        error = labelMat - sigmoid(dataMatrix * weights)        # 计算预测结果与实际值的偏差
+        weights += alpha * dataMatrix.transpose() * error       # 梯度上升迭代计算权值（行列转置）
+    return weights                                              # 把权值矩阵转化为数组
 
 
 def plotBestFit(weights):
@@ -75,94 +71,85 @@ def plotBestFit(weights):
     plt.show()
 
 
-def stocGradAscent0(dataMatrix, classLabels):
+def stocGradAscent0(dataMatrix, classLabels, alpha=0.01):
     """
-    随机梯度上升法：增量更新（每次仅用一个样本点更新回归系数），在线学习算法
+    随机梯度上升法：增量更新（每次仅用一个样本点、即一行更新回归系数），在线学习算法
     :param dataMatrix: 
     :param classLabels: 
     :return: 
     """
 
     m, n = shape(dataMatrix)
-    alpha = 0.01
-    weights = ones(n)       # initialize to all ones
-    # TODO 求梯度gradient = (classLabels[i] - h) * (classLabels[i] - sigmoid(sum(dataMatrix[i] * weights)))
+    weights = ones(n)
     for i in range(m):
-        h = sigmoid(sum(dataMatrix[i] * weights))       # 逐行取训练矩阵为向量，和权值向量的点积置入sigmoid函数
-        error = classLabels[i] - h
-        weights = weights + alpha * error * dataMatrix[i]
+        error = classLabels[i] - sigmoid(sum(dataMatrix[i] * weights))
+        weights += alpha * error * dataMatrix[i]            # 根据偏差调整权值
     return weights
 
 
+def stocGradAscent1(dataMatrix, classLabels, numIter=30):
+    """
+    改进的随机梯度上升法：1，多次迭代；2，随机抽取；3，根据迭代次数修改学习率
+    :param dataMatrix: 
+    :param classLabels: 
+    :param numIter:         迭代次数
+    :return: 
+    """
 
-def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     m, n = shape(dataMatrix)
-    weights = ones(n)  # initialize to all ones
-    for j in range(numIter):
-        dataIndex = range(m)
-        for i in range(m):
-            alpha = 4 / (1.0 + j + i) + 0.0001  # alpha decreases with iteration, does not
-            randIndex = int(random.uniform(0, len(dataIndex)))  # go to 0 because of the constant
-            h = sigmoid(sum(dataMatrix[randIndex] * weights))
-            error = classLabels[randIndex] - h
-            weights = weights + alpha * error * dataMatrix[randIndex]
-            del (dataIndex[randIndex])
+    weights = ones(n)
+    for j in range(numIter):                    # 迭代计算numIter次
+        dataIndex = list(range(m))
+        random.shuffle(dataIndex)               # 打乱样本随机抽取
+        for i in dataIndex:                     # 逐行样本插入训练
+            alpha = 4 / (1.0 + j + i) + 0.0001  # 学习率随迭代次数和样本数增加而减小（当j<<max(i)时，alpha就不是严格下降的）
+            error = classLabels[i] - sigmoid(sum(dataMatrix[i] * weights))
+            weights += + alpha * error * dataMatrix[i]
     return weights
-
-
-dataArr, labelMat = loadDataSet()
-w = stocGradAscent0(dataArr, labelMat)
-
-# w = gradAscent(dataArr, labelMat)
-plotBestFit(w)
 
 
 def classifyVector(inX, weights):
-    prob = sigmoid(sum(inX * weights))
-    if prob > 0.5:
-        return 1.0
-    else:
-        return 0.0
+    """ 分类器函数 """
+    return 1 if sigmoid(sum(inX * weights)) > 0.5 else 0
 
 
+# 测试：从疝气病症预测病马死亡率
 def colicTest():
-    frTrain = open('horseColicTraining.txt')
-    frTest = open('horseColicTest.txt')
-    trainingSet = [];
+    """ 预测病马死亡率 """
+    trainingSet = []
     trainingLabels = []
-    for line in frTrain.readlines():
+    for line in open('horseColicTraining.txt').readlines():
         currLine = line.strip().split('\t')
-        lineArr = []
-        for i in range(21):
-            lineArr.append(float(currLine[i]))
+        lineArr = [float(currLine[i]) for i in range(21)]
         trainingSet.append(lineArr)
-        trainingLabels.append(float(currLine[21]))
-    trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 1000)
-    errorCount = 0;
-    numTestVec = 0.0
-    for line in frTest.readlines():
+        trainingLabels.append(float(currLine[-1]))
+
+    trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 1000)    # 随机梯度下降计算logist回归函数权值
+    errorCount, numTestVec = 0, 0.0
+    for line in open('horseColicTest.txt').readlines():
         numTestVec += 1.0
         currLine = line.strip().split('\t')
-        lineArr = []
-        for i in range(21):
-            lineArr.append(float(currLine[i]))
-        if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+        lineArr = [float(currLine[i]) for i in range(21)]
+        if classifyVector(array(lineArr), trainWeights) != int(currLine[-1]):        # 统计分类失败次数
             errorCount += 1
-    errorRate = (float(errorCount) / numTestVec)
-    print
-    "the error rate of this test is: %f" % errorRate
+    errorRate = errorCount / numTestVec
+    print("the error rate of this test is: {0}".format(errorRate))
     return errorRate
 
 
 def multiTest():
-    numTests = 10;
-    errorSum = 0.0
-    for k in range(numTests):
-        errorSum += colicTest()
-    print
-    "after %d iterations the average error rate is: %f" % (numTests, errorSum / float(numTests))
+    """ 统计多次测试的失败率 """
+
+    numTests, errorSum = 10, 0.0
+    errorSum = sum([colicTest() for _ in range(numTests)])
+    print("after %d iterations the average error rate is: {0}".format(numTests, errorSum / float(numTests)))
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+
     # dataArr, labelMat = loadDataSet()
-    # gradAscent(dataArr, labelMat)
+    # w = stocGradAscent1(dataArr, labelMat)
+
+    # w = gradAscent(dataArr, labelMat)
+    # plotBestFit(w)
+    multiTest()
--- perceptron.py
+++ perceptron.py
@@ -1,25 +1,28 @@
 from numpy import *
 
 
-def classfy(dataSet, classList, learningRate=1):
+def classfy(dataSet, classList, learningRate=1, numIter=20):
     trainSetSize, dim = len(dataSet), len(dataSet[0])
-    w, b = zeros((dim)), 0
-    while 1:
-        flag = True                                 # 修正标记：当遍历完所有样本都没有修改则完成训练
+    w, b, errorRate = zeros((dim)), 0, 1
+    for _ in range(numIter):                        # 迭代训练数据
+        flag = True                                 # 修正标记：当遍历完所有样本都无须修改则表示完成训练
+        errorCount = 0.0
         for i in range(trainSetSize):               # 逐行数据带入判别式，判断预测分类与实际分类是否相符，直到每行都分类正确
             res = classList[i] * (dot(array(dataSet[i]), w) + b)  # dot：求向量内积
             if res <= 0:                            # 实际与预测结果异号即错判：梯度下降更新w、b（公式见《统计学习方法》P29）
                 flag = False
+                errorCount += 1.0
                 w += learningRate * classList[i] * array(dataSet[i])
                 b += learningRate * classList[i]
-                # print("w = {0}\tb = {1}".format(w, b))
-                break
+        errorRate = errorCount / trainSetSize
         if flag:
             break
-    return lambda x: -1 if dot(w, array(x)) + b < 0 else 1  # 返回感知机模型函数：f(x) = sign(w * x + b)
+    f = lambda x: -1 if dot(w, array(x)) + b < 0 else 1
+    return f, errorRate     # 返回感知机模型函数及错误率：f(x) = sign(w * x + b)
 
 
 if __name__ == "__main__":
-    dataSet = [[3, 3], [4, 3], [1, 1]]
-    classList = [1, 1, -1]
-    f = classfy(dataSet, classList)
+    dataSet = [[3, 3], [4, 3], [3, 1], [0, 0], [2, 0], [0, 2], [3, 0], [2, 5]]
+    classList = [1, 1, -1, 1, -1, 1, -1, 1]
+    f, errorRate = classfy(dataSet, classList)
+    print(f([0, -1]), errorRate)
